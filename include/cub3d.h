5b27fae492a17170dad2cf0ae8869cee2c2f789a
rachid oudouch <rashidoudouch@gmail.com>
2023-02-02 15:54:29 +0100

    add gitignore

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,13 +6,14 @@
 /*   By: roudouch <roudouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 15:34:13 by ahjadani          #+#    #+#             */
-/*   Updated: 2023/02/02 15:37:48 by roudouch         ###   ########.fr       */
+/*   Updated: 2023/02/02 15:53:26 by roudouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "../libft/libft.h"
 # include <mlx.h>
 # include <fcntl.h>
 # include <math.h>
