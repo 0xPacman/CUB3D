65e22772d8acd1e336234ed1519e1d2ff610d83b
rachid oudouch <rashidoudouch@gmail.com>
2023-03-15 14:13:28 +0100

    done

--- srcs/execution/controls/doors/change_status_door.c
+++ srcs/execution/controls/doors/change_status_door.c
@@ -6,14 +6,14 @@
 /*   By: roudouch <roudouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 13:03:09 by roudouch          #+#    #+#             */
-/*   Updated: 2023/03/15 14:06:59 by roudouch         ###   ########.fr       */
+/*   Updated: 2023/03/15 14:12:20 by roudouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../../include/cub3d.h"
 
 int press_key_f(t_engine *engine) {
-    if (engine->door_pos.x != -1 && engine->door_pos.y != -1 && engine->can_open_door)
+    if (engine->door_pos.x != -1 && engine->door_pos.y != -1)
     {
         if (engine->map[(int)engine->door_pos.y][(int)engine->door_pos.x] == 'D')
             engine->map[(int)engine->door_pos.y][(int)engine->door_pos.x] = 'O';
